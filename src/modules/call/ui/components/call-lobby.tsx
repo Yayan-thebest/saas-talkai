080bd54691b54a2680758c2fdb77d9a846708795
Yayan-thebest <monedgard@yahoo.com>
2025-06-06 00:24:21 -0400

    22:video call

---
+++ src/modules/call/ui/components/call-lobby.tsx
@@ -0,0 +1,85 @@
+import { Button } from '@/components/ui/button';
+import { authClient } from '@/lib/auth-client';
+import { generateAvatarUri } from '@/lib/avatar';
+import { DefaultVideoPlaceholder, StreamVideoParticipant, useCallStateHooks, VideoPreview, ToggleAudioPreviewButton, ToggleVideoPreviewButton } from '@stream-io/video-react-sdk';
+import '@stream-io/video-react-sdk/dist/css/styles.css';
+import { CameraIcon, LogInIcon, MicIcon } from 'lucide-react';
+import Link from 'next/link';
+
+
+
+interface Props {
+    onJoin: () => void;
+}
+
+const DisabledVideoPreview = () => {
+    const { data } = authClient.useSession();
+
+    return (
+        <DefaultVideoPlaceholder
+            participant={
+                 {
+                    name: data?.user.name ?? "",
+                    image: data?.user.image ?? generateAvatarUri({ seed: data?.user.name ?? "", variant: "initials"})
+                 } as StreamVideoParticipant
+            }
+        />
+    )
+};
+
+
+const AllowBrowserPermissions = () => {
+    return (
+        <div className='text-sm'>
+            Please grant your browser a permission to access your <CameraIcon/> camera and <MicIcon/> microphone
+
+        </div>
+    )
+}
+
+export const CallLobby = ({ onJoin }: Props) => {
+
+    const { useCameraState, useMicrophoneState } = useCallStateHooks();
+
+    const { hasBrowserPermission: hasMicPermission } = useMicrophoneState();
+    const { hasBrowserPermission: hasCameraPermission } = useCameraState();
+
+    const hasBrowserMediaPermission = hasCameraPermission && hasMicPermission;
+
+
+    return (
+        <div className='min-h-screen flex flex-col items-center justify-center bg-radial from-sidebar-accent to-sidebar'>
+
+            <div className='py-4 px-8 flex flex-1 items-center justify-center'>
+                <div className='flex flex-col items-center justify-center gap-y-6 bg-background rounded-lg p-10 shadow-sm'>
+                  <div className='flex flex-col gap-y-2 text-center'>
+                    <h6 className='text-lg font-medium'>Ready to join?</h6>
+                    <p className='text-sm'>Setup your call before joining</p>
+                  </div>  
+                  <VideoPreview
+                    DisabledVideoPreview={
+                        hasBrowserMediaPermission ? DisabledVideoPreview : AllowBrowserPermissions
+                    }
+                  />
+                  <div className='flex gap-x-2'>
+                      <ToggleAudioPreviewButton /> 
+                      <ToggleVideoPreviewButton/> 
+                  </div>
+                  <div className='flex gap-x-2 justify-between w-full'>
+                    <Button asChild variant={"outline"}>
+                        <Link href="/meetings">
+                            Cancel
+                        </Link>
+                    </Button>
+                    <Button onClick={onJoin}>
+                        <LogInIcon/> Join call
+                    </Button>
+
+                  </div>
+                </div>
+            </div> 
+        </div>
+
+    )
+}
+
--- src/modules/meetings/server/procedures.ts
+++ src/modules/meetings/server/procedures.ts
@@ -7,6 +7,8 @@ import { TRPCError } from "@trpc/server";
 import { agents, meetings } from "@/db/schema";
 import { meetingsInsertSchema, meetingsUpdateSchema } from "../schema";
 import { MeetingStatus } from "../types";
+import { streamVideo } from "@/lib/stream-video";
+import { generateAvatarUri } from "@/lib/avatar";
 
 
 /**
@@ -14,6 +16,29 @@ import { MeetingStatus } from "../types";
  * avec des protections et validations selon les cas.
  */
 export const meetingsRouter = createTRPCRouter({
+
+    generateToken: protectedProcedure.mutation(async ({ ctx }) => {
+        await streamVideo.upsertUsers([
+            {
+                id: ctx.auth.user.id,
+                name: ctx.auth.user.name,
+                role: "admin",
+                image: 
+                    ctx.auth.user.image ??  generateAvatarUri({ seed: ctx.auth.user.name, variant: "initials"})
+            },
+        ]);
+
+        const expirationTime = Math.floor(Date.now() / 1000) + 3600; // 1 hour
+        const issuedAt = Math.floor(Date.now() / 1000) - 60;
+
+        const token = streamVideo.generateUserToken({
+            user_id: ctx.auth.user.id,
+            exp: expirationTime,
+            validity_in_seconds: issuedAt,
+        });
+
+        return token;
+    }),
 
     remove: protectedProcedure
         .input(z.object({id: z.string()}))
@@ -67,10 +92,54 @@ export const meetingsRouter = createTRPCRouter({
         .values({...input, userId: ctx.auth.user.id})
         .returning();
 
-        // TODO: create stream call, upsert stream users
-        
+        // create stream call every time a new meeting is created, upsert stream users
+        const call = streamVideo.video.call("default", createdMeeting.id);
+        await call.create({
+            data: {
+                created_by_id: ctx.auth.user.id,
+                custom: {
+                    meetingId: createdMeeting.id,
+                    meetingName: createdMeeting.name,
+                },
+                settings_override: {
+                    transcription: {
+                        language: "en",
+                        mode: "auto-on",
+                        closed_caption_mode: "auto-on",
+                    },
+                    recording: {
+                        mode: "auto-on",
+                        quality: "1080p",
+                    },
+                },
+            },
+        });
 
-    return createdMeeting;
+
+        const [existingAgent] = await db
+            .select()
+            .from(agents)
+            .where(eq(agents.id, createdMeeting.agentId));
+        
+            if(!existingAgent){
+                throw new TRPCError({
+                    code: "NOT_FOUND",
+                    message: "Agent not found",
+                })
+            }
+
+            await streamVideo.upsertUsers([
+                {
+                    id: existingAgent.id,
+                    name: existingAgent.name,
+                    image: generateAvatarUri({
+                        seed: existingAgent.name,
+                        variant: "botttsNeutral",
+                    }),
+                },
+            ]);
+
+        return createdMeeting;
 
     }),  
     // si une page n'a pas l'option de,login(par oublie) et qu'un user se rend sur la page via lien
