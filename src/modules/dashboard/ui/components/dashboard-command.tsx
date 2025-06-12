0f7eed7ff9ea0b939fe2fab90a93acbd1aa554a2
Yayan-thebest <monedgard@yahoo.com>
2025-06-12 13:15:26 -0400

    nextconfig redirects

--- src/modules/dashboard/ui/components/dashboard-command.tsx
+++ src/modules/dashboard/ui/components/dashboard-command.tsx
@@ -24,49 +24,66 @@ export const DashboardCommand = ({open, setOpen}: Props) =>{
 
 
     return (
-        <CommandResponsiveDialog shouldFilter={false} open={open} onOpenChange={setOpen}>
+        <CommandResponsiveDialog
+            shouldFilter={false}
+            open={open}
+            onOpenChange={setOpen}
+        >
             <CommandInput
-                placeholder="Find a meeting or agent..."
-                value={search}
-                onValueChange={(value) => setSearch(value)}
+            placeholder="Find a meeting or agent..."
+            value={search}
+            onValueChange={(value) => setSearch(value)}
             />
             <CommandList>
-                <CommandGroup heading="Meetings">
-                    <CommandEmpty>
-                        <span className="text-muted-foreground text-sm">
-                            No meetings found
-                        </span>
-                    </CommandEmpty>
-                    {meetings.data?.items.map((meeting) => (
-                        <CommandItem
-                            key={meeting.id}
-                            onSelect={() => {router.push(`/meetings/${meeting.id}`); setOpen(false);}}
-                        >
-                            {meeting.name}
-                        </CommandItem>
-                    ))}
-                </CommandGroup>
-                <CommandGroup heading="Agents">
-                    <CommandEmpty>
-                        <span className="text-muted-foreground text-sm">
-                            No agents found
-                        </span>
-                    </CommandEmpty>
-                    {agents.data?.items.map((agent) => (
-                        <CommandItem
-                            key={agent.id}
-                            onSelect={() => {router.push(`/agents/${agent.id}`); setOpen(false);}}
-                        >
-                            <GeneratedAvatar
-                                seed={agent.name}
-                                variant="botttsNeutral"
-                                className="size-5"
-                            />
-                            {agent.name}
-                        </CommandItem>
-                    ))}
-                </CommandGroup>
+            <CommandGroup heading="Meetings">
+                {meetings.data?.items?.length ? (
+                meetings.data.items.map((meeting) => (
+                    <CommandItem
+                    key={meeting.id}
+                    onSelect={() => {
+                        router.push(`/meetings/${meeting.id}`);
+                        setOpen(false);
+                    }}
+                    >
+                    {meeting.name}
+                    </CommandItem>
+                ))
+                ) : (
+                <CommandEmpty>
+                    <span className="text-muted-foreground text-sm">
+                    No meetings found
+                    </span>
+                </CommandEmpty>
+                )}
+            </CommandGroup>
+
+            <CommandGroup heading="Agents">
+                {agents.data?.items?.length ? (
+                agents.data.items.map((agent) => (
+                    <CommandItem
+                    key={agent.id}
+                    onSelect={() => {
+                        router.push(`/agents/${agent.id}`);
+                        setOpen(false);
+                    }}
+                    >
+                    <GeneratedAvatar
+                        seed={agent.name}
+                        variant="botttsNeutral"
+                        className="size-5 mr-2"
+                    />
+                    {agent.name}
+                    </CommandItem>
+                ))
+                ) : (
+                <CommandEmpty>
+                    <span className="text-muted-foreground text-sm">
+                    No agents found
+                    </span>
+                </CommandEmpty>
+                )}
+            </CommandGroup>
             </CommandList>
         </CommandResponsiveDialog>
-    )
+    );
 }
